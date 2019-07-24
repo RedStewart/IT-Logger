23b275a90461866ab3c7b9f23100816a8a663fe7
Reed Stewart <reedostewart@gmail.com>
2019-07-24 12:02:12 +1200

    Can now search the logs for text or by technician. Done differently to the contact keeper where it would filter the state, this uses the json-placeholder api to make request each time the user types. Also init for the tech state

---
+++ src/reducers/techReducer.js
@@ -0,0 +1,12 @@
+const intialState = {
+  techs: null,
+  loading: false,
+  error: null
+};
+
+export default (state = intialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
